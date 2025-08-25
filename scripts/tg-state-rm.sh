538ca4d317ffa68d1bae8641175796080ce5c655
Sebastian Klamar <git@sebastian.klamar.name>
2025-08-25 17:14:30 +0200

    feat(tg-state-rm): remove all states except Proxmox
    this enables destroying the VMs in Proxmox also when the VMs are offline or
    when the cluster is not in a proper state any longer -- which happens more
    often in a test-'n-trial phase

--- scripts/tg-state-rm.sh
+++ scripts/tg-state-rm.sh
@@ -6,3 +6,11 @@ terragrunt state rm 'module.volumes.module.persistent-volume["pv-mongodb"].kuber
 terragrunt state rm 'module.volumes.module.proxmox-volume["pv-mongodb"].restapi_object.proxmox-volume'
 terragrunt state rm 'module.sealed_secrets.kubernetes_namespace.sealed-secrets'
 terragrunt state rm 'module.sealed_secrets.kubernetes_secret.sealed-secrets-key'
+terragrunt state rm 'module.talos.talos_cluster_kubeconfig.this'
+terragrunt state rm 'module.talos.talos_machine_bootstrap.this'
+terragrunt state rm 'module.talos.talos_machine_secrets.this'
+terragrunt state rm 'module.talos.talos_image_factory_schematic.updated'
+terragrunt state rm 'module.talos.talos_image_factory_schematic.this'
+
+#terragrunt state rm 'module.talos.talos_machine_configuration_apply.this["dbg-ctrl-01.test.iseja.net"]''
+#terragrunt state rm 'module.talos.talos_machine_configuration_apply.this["dbg-work-02.test.iseja.net"]'
