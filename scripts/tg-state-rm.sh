60f794e898f3b08d2c86f3c54c1c23e5724dd720
Sebastian Klamar <git@sebastian.klamar.name>
2025-12-11 22:32:52 +0100

    fix(tg-state-rm): correct proxmox volume handling and cleanup logic
    - fixed a bug where proxmox volumes did not get hindered from deletion
    - better documentation

--- scripts/tg-state-rm.sh
+++ scripts/tg-state-rm.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-for i in $(terragrunt state list | grep module.volumes.module.persistent-volume); do terragrunt state rm "$i"; done
-# only delete proxmox volumes if we don't want to keep the data
-#for i in $(terragrunt state list | grep module.volumes.module.proxmox-volume; do terragrunt state rm "$i"; done
+# also state of proxmox volumes so it's not touched by a `tofu/terragrunt destroy`` command
+for i in $(terragrunt state list | grep module.volumes.module.proxmox-volume; do terragrunt state rm "$i"; done
 
-terragrunt state rm 'module.proxmox_csi_plugin.kubernetes_secret.proxmox-csi-plugin'
-terragrunt state rm 'module.proxmox_csi_plugin.kubernetes_namespace.csi-proxmox'
+# also keep some other cluster data by removing it from state tracking (it will be readded by refresh)
 terragrunt state rm 'module.sealed_secrets.kubernetes_namespace.sealed-secrets'
 terragrunt state rm 'module.sealed_secrets.kubernetes_secret.sealed-secrets-key'
 terragrunt state rm 'module.talos.talos_cluster_kubeconfig.this'
-terragrunt state rm 'module.talos.talos_machine_bootstrap.this'
 terragrunt state rm 'module.talos.talos_machine_secrets.this'
 terragrunt state rm 'module.talos.talos_image_factory_schematic.updated'
 terragrunt state rm 'module.talos.talos_image_factory_schematic.this'
 
-#terragrunt state rm 'module.talos.talos_machine_configuration_apply.this["dbg-ctrl-01.test.iseja.net"]''
-#terragrunt state rm 'module.talos.talos_machine_configuration_apply.this["dbg-work-02.test.iseja.net"]'
+# remove everything which would block a destroy command when the cluster is not functional
+# it get's destroyed by other means anyway (VM destruction)
+terragrunt state rm 'module.proxmox_csi_plugin.kubernetes_secret.proxmox-csi-plugin'
+terragrunt state rm 'module.proxmox_csi_plugin.kubernetes_namespace.csi-proxmox'
+terragrunt state rm 'module.talos.talos_machine_bootstrap.this'
