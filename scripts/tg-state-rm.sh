9e86a825aff4c7aa40397a4ada6ad4153feff199
Sebastian Klamar <git@sebastian.klamar.name>
2025-12-13 22:17:55 +0100

    fix(tg-state-rm): add missing end brace

--- scripts/tg-state-rm.sh
+++ scripts/tg-state-rm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# also state of proxmox volumes so it's not touched by a `tofu/terragrunt destroy`` command
-for i in $(terragrunt state list | grep module.volumes.module.proxmox-volume; do terragrunt state rm "$i"; done
+# remove state of proxmox volumes so it's not touched by a `tofu/terragrunt destroy`` command
+for i in $(terragrunt state list | grep module.volumes.module.proxmox-volume); do terragrunt state rm "$i"; done
 
 # also keep some other cluster data by removing it from state tracking (it will be readded by refresh)
 terragrunt state rm 'module.sealed_secrets.kubernetes_namespace.sealed-secrets'
@@ -12,7 +12,7 @@ terragrunt state rm 'module.talos.talos_image_factory_schematic.updated'
 terragrunt state rm 'module.talos.talos_image_factory_schematic.this'
 
 # remove everything which would block a destroy command when the cluster is not functional
-# it get's destroyed by other means anyway (VM destruction)
+# it gets destroyed by other means anyway (VM destruction)
 terragrunt state rm 'module.proxmox_csi_plugin.kubernetes_secret.proxmox-csi-plugin'
 terragrunt state rm 'module.proxmox_csi_plugin.kubernetes_namespace.csi-proxmox'
 terragrunt state rm 'module.talos.talos_machine_bootstrap.this'
