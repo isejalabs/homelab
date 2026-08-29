3908e0473a6d5bd6fb0f37f3285571964d760f71
Sebastian Klamar <git@sebastian.klamar.name>
2026-08-29 08:10:39 +0200

    chore(non-prod): update talos 1.12.11, k8s 1.34.11
    update non-prod env.s to talos v1.12.11 and kuberbernetes to 1.34.11
    
    also refactor upgrade-k8s.sh script by using a variable for the command

--- scripts/upgrade-k8s.sh
+++ scripts/upgrade-k8s.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 TALCONFIG=$(find . -iname talos-config.yaml)
+K8S_VERSION="1.34.11" # renovate: github-releases=kubernetes/kubernetes
 
 # Usage: upgrade-k8s.sh [--dry-run]
-talosctl upgrade-k8s $1 --talosconfig ${TALCONFIG} --nodes $( yq -r '.contexts.*.endpoints.[0]' ${TALCONFIG}) --to 1.34.5 # renovate: github-releases=kubernetes/kubernetes
+talosctl upgrade-k8s $1 --talosconfig ${TALCONFIG} --nodes $( yq -r '.contexts.*.endpoints.[0]' ${TALCONFIG}) --to ${K8S_VERSION}
